e7ade52b1d351c891cc78a1618825d1a6403e4c9
Henry Jia <henryjia18@yahoo.com>
2015-04-09 23:27:32 +0100

    Adds some R visualisations of the distribution of Y for creating an activation function

--- neural2/distY.R
+++ neural2/distY.R
@@ -1,5 +1,7 @@
 distY <- function()
 {
     Y = read.csv(file = "trainYP2_1.csv", header = FALSE)
+    td = data.frame(table(Y))
     hist(x = table(Y), breaks = 100)
+    lines(x = 1:max(Y), y = (2000 / (1:max(Y) + 20)))
 }
