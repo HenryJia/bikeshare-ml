c779fd96c9a5aec3f1d7c8395362165e9ca31e40
Hengjian (Henry) Jia <henryjia18@yahoo.com>
2015-04-27 16:57:26 +0000

    Roll back to commit a9caf90352fc63f76d8868d5ce59f93b5df5e368 due to latest commit overfitting the trainingset.

--- README.md
+++ README.md
@@ -7,35 +7,26 @@ However, there are still many different implementations of the neural network, t
 
 logNeural branch -> a natural log activate neural network, 3 layers
 
-JA = 5903.7
-JAValidate = 5258.1
-JB = 1.5014e+04
-JBValidate = 1.4295e+04
+JA = 5903.7 JAValidate = 5258.1 JB = 1.5014e+04 JBValidate = 1.4295e+04
 
 An version 1 of the sigmoidNeural branch -> sigmoid function activated neural network, 3 layers
 
-JA =  5502.1
-JB =    1.4910e+04
-JAValidate =  4954.1
-JBValidate =    1.3835e+04
+JA = 5502.1 JB = 1.4910e+04 JAValidate = 4954.1 JBValidate = 1.3835e+04
 
 The version 2 of the sigmoidNeural branch -> same as the version 1 but the output unit is not activated, 3 layers
 
-JA = 1534.7
-JAValidate = 1635.3
-JB = 3757.5
-JBValidate = 3886.7
+JA = 1534.7 JAValidate = 1635.3 JB = 3757.5 JBValidate = 3886.7
 
 The version 1 of the sigmoidNeural3+ branch -> sigmoid activated neural network just like the version 2 of the sigmoidNeural branch but with more layers than 3
 
-JA =  562.68
-JAValidate =  743.38
-JB =  1071.0
-JBValidate =  1370.4
+JA = 562.68 JAValidate = 743.38 JB = 1071.0 JBValidate = 1370.4
 
 The version 2 of the sigmoidNeural3+ branch -> sigmoid activated neural network just like the version 2 of the sigmoidNeural3+ branch but with 16 output units instead of 1. The end result of the output units are averaged.
 
-JA =  330.74
-JAValidate =  666.86
-JB =  673.13
-JBValidate =  1222.8
+JA = 330.74 JAValidate = 666.86 JB = 673.13 JBValidate = 1222.8
+
+The version 3 of the sigmoidNeural3+ branch -> Same as version 2 but bigger and also with one more input layer.
+
+JA = 223.8534764672244 JAValidate = 670.2424706468234 JB = 504.2598015271401 JBValidate = 1160.346418306346
+
+However, due to the fact that version 3 was overfitting the training set, I rolled it back to version 2 on the sigmoidNeural3+ branch.
--- neural2/neural2_2.m
+++ neural2/neural2_2.m
@@ -5,20 +5,20 @@ function [ThetaB1, ThetaB2, ThetaB3] = neural2_2
 
 fprintf('Load Data\n');
 
-Xa = load("trainP7_1.csv");
-Xb = load("trainP7_2.csv");
+Xa = load("trainP7_1.csv")(:, [1, 3:end]);
+Xb = load("trainP7_2.csv")(:, [1, 3:end]);
 Ya = load("trainYP2_1.csv" );
 Yb = load("trainYP2_2.csv" );
-validateXa = load("validateP7_1.csv");
-validateXb = load("validateP7_2.csv");
+validateXa = load("validateP7_1.csv")(:, [1, 3:end]);
+validateXb = load("validateP7_2.csv")(:, [1, 3:end]);
 validateYa = load("validateY1.csv");
 validateYb = load("validateY2.csv");
-testXa = load("original/testPF1_1.csv");
-testXb = load("original/testPF1_2.csv");
+testXa = load("original/testPF1_1.csv")(:, [1, 3:end]);
+testXb = load("original/testPF1_2.csv")(:, [1, 3:end]);
 
 % Set Important Variables:
 
-alpha = 0.005;
+alpha = 0.015;
 lambda = 0;
 iters = 30000;
 scatterIters = 750;
@@ -57,14 +57,14 @@ testXb_norm = [ones(size(testXb_norm, 1) ,1), testXb_norm];
 fprintf('Features Normalised. Initialise Thetas. Press Enter\n');
 %pause;
 
-ThetaA1 = abs(randInitializeWeights(11, 55));
-ThetaB1 = abs(randInitializeWeights(11, 55));
+ThetaA1 = abs(randInitializeWeights(10, 40));
+ThetaB1 = abs(randInitializeWeights(10, 40));
 
-ThetaA2 = abs(randInitializeWeights(55, 275));
-ThetaB2 = abs(randInitializeWeights(55, 275));
+ThetaA2 = abs(randInitializeWeights(40, 160));
+ThetaB2 = abs(randInitializeWeights(40, 160));
 
-ThetaA3 = abs(randInitializeWeights(275, 28));
-ThetaB3 = abs(randInitializeWeights(275, 28));
+ThetaA3 = abs(randInitializeWeights(160, 1));
+ThetaB3 = abs(randInitializeWeights(160, 1));
 
 % Calculate Thetas & Results For First Hidden Layer
 
@@ -132,7 +132,6 @@ JBValidate += (sum(sum(ThetaB1(2:end, :) .^ 2)) + sum(sum(ThetaB2(2:end, :) .^ 2
 predictYbTest = forwardPropagate(testXb_norm, ThetaB1, ThetaB2, ThetaB3, Yb);
 
 csvwrite("result2_2.csv", predictYbTest);
-csvwrite("JB.csv", [JB; JBValidate]);
 
 fprintf('Done. Press Enter\n');
 %pause;
